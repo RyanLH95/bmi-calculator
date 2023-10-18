ed307448d33647c7907fd3412ec00e432014ccc0
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-18 15:23:06 +0100

    Add a 'container' <div> inside of 'app' <div> and input a <h2> tag with 'BMI Calculator' as the title, then create a <form> tag and input <div>, <label> and <input> related to height and weight.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,23 @@ function App() {
   return (
     <>
       <div className="App">
-
+        <div className="container">
+          <h2 className="center">BMI Calculator</h2>
+          <form>
+            <div>
+              <label>Height (cm)</label>
+              <input defaultValue={height} />
+            </div>
+            <div>
+              <label>Weight (kg)</label>
+              <input defaultValue={weight}></input>
+            </div>
+            <div>
+              <button className="btn" type="submit">Submit</button>
+              <button className="btn btn-outline" type="submit">Reload</button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   )

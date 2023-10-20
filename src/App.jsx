55794b9d7f3a38b6bcb59bdd8d8b9f0b442419c3
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-20 15:04:51 +0100

    Change 'defaultValue'attributes within input to 'value', then add an onChange attribute with the value of arrow function for 'measurement(event.target.value)'

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,32 @@ function App() {
 
   let calculate = (event) => {
     // This is here to prevent an invalid input
-    event.preventDefault()
+    event.preventDefault();
 
     if (weight === 0 || height === 0) {
       alert('A valid number must be entered to proceed with calculation')
     } else {
-      let bmi = (weight / (height * height) * 10000)
-      setBmi(bmi.toFixed(1))
+      let result = (weight / (height * height) * 10000)
+      setResult(result.toFixed(1))
     }
   }
-
+  
   return (
     <>
       <div className="app">
         <div className="container">
           <h2 className="center">BMI Calculator</h2>
-          <form>
+          <form id="bmi-form"
+                onSubmit={calculate}>
             <div>
-              <label>Height (cm)</label>
-              <input defaultValue={height} />
+              <label>Height (cm)
+                <input value={height} onChange={(event) => setHeight(event.target.value)}/> 
+              </label>
             </div>
             <div>
-              <label>Weight (kg)</label>
-              <input defaultValue={weight}></input>
+              <label>Weight (kg)
+                <input value={weight} onChange={(event) => setWeight(event.target.value)}></input>
+              </label>
             </div>
             <div>
               <button className="btn" type="submit">Submit</button>
